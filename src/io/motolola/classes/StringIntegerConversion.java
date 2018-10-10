a626417b46742f34b62412aec5694e6c9515788c
Motolola <motolola@icloud.com>
2018-10-10 02:26:27 +0100

    String reverse

--- src/io/motolola/classes/StringIntegerConversion.java
+++ src/io/motolola/classes/StringIntegerConversion.java
@@ -1,11 +1,16 @@
 package io.motolola.classes;
 
+import java.util.Arrays;
+
 public class StringIntegerConversion {
 
     public static void main(String[] args)
     {
         System.out.println(intToString(345));
         System.out.println(stringToInt("348"));
+        System.out.println(strReverse("boy"));
+        System.out.println(strReverse2("Congratulations"));
+
     }
 
     public static String intToString(int number)
@@ -16,5 +21,19 @@ public class StringIntegerConversion {
     {
         //return Integer.valueOf(str); Also a goog choice.
         return Integer.parseInt(str);
+    }
+    public static String strReverse(String str){
+        StringBuilder sb = new StringBuilder(str);
+        return sb.reverse().toString();
+    }
+    public static String strReverse2(String str)
+    {
+        char[] arr = str.toCharArray();
+        int index = str.length() - 1;
+        String rev = "";
+        for (int i = index; i >= 0; i-- ){
+            rev += str.charAt(i);
+        }
+        return rev;
     }
 }
